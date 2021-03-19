90a243312943c40ea65f625205f51023f63a38c8
Dakota Taylor <dt900319@ohio.edu>
2021-03-19 19:01:30 -0400

    Added implementation to C# math-based solution

--- sum_of_squares/SumOfPerfectSquares-math.cs
+++ sum_of_squares/SumOfPerfectSquares-math.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 public class SumOfSquares {
+    public static void Main(string[] args) {
+        int max = 1000001;
+        TimeSpan start = DateTime.Now.TimeOfDay;
+        TimeSpan lastDuration = TimeSpan.Zero;
+        TimeSpan lastElapsed = TimeSpan.Zero;
+
+        for (int i = 1; i < max; ++i) {
+            TimeSpan t1 = DateTime.Now.TimeOfDay;
+            int s = NSquaresFor(i);
+            TimeSpan t2 = DateTime.Now.TimeOfDay;
+            if (t2 - t1 > TimeSpan.FromTicks(lastDuration.Ticks * 2) || t2 - start > TimeSpan.FromTicks(lastElapsed.Ticks * 2)) {
+                lastDuration = t2 - t1;
+                lastElapsed = t2 - start;
+                Console.WriteLine($"{i}: sum of {s} perfect squares: computed in {lastDuration}. Elapsed time: {lastElapsed}.");
+            }
+        }
+        TimeSpan end = DateTime.Now.TimeOfDay;
+        Console.WriteLine($"Total elapsed time to computer sum of squares up to {max}: {end - start}.");
+    }
+
     // Uses Legendre's three-square theorem and Lagrange's four-square theorem
     public static int NSquaresFor(int n) {
         int x = (int)Math.Sqrt(n);
         if (x * x == n) return 1; // n is already a perfect square
-        if (x * x == n + 1) return 2; // n is one above a perfect square
+        if (x * x == n + 1) return 2; // n is a perfect square plus 1
 
         // Checking if n is in the form of 4^a(8b+7). If it is, then it must be expressed in four squares
         while (n % 4 == 0) n /= 4;
